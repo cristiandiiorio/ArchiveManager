c3ad2b6e139bca73a677e0634198cd90ff98fc72
cristiandiiorio <112891194+cristiandiiorio@users.noreply.github.com>
2022-12-11 18:24:19 +0100

    Add files via upload

--- acListener.java
+++ acListener.java
@@ -34,49 +34,37 @@ public class acListener implements ActionListener{
                     String seriesLink = null;
                     boolean seriesStatus;
                     
-                    boolean escape = false;
-                    
-                    
-                    while(!escape){
-                        while(seriesName == null){
-                            seriesName = JOptionPane.showInputDialog("Series' name: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
-                            if(!(seriesName == null) && (seriesName.equals("*"))){
-                                escape = true;
-                            }
-                        }
-                        while(seriesStatusPre == null && !escape){
-                            seriesStatusPre = JOptionPane.showInputDialog("Series' status: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
-                            if(!(seriesStatusPre == null) && (seriesName.equals("*"))){
-                                escape = true;
-                            }
-                        }
-                        while(seriesLink == null && !escape){
-                            seriesLink = JOptionPane.showInputDialog("Series' link: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
-                            if(!(seriesLink == null) && (seriesName.equals("*"))){
-                                escape = true;
-                            }
-                        }
-                    }
-                    
-                    if(!escape){
-                        seriesStatus = Boolean.parseBoolean(seriesStatusPre);
-                        TVSeries tvseries = new TVSeries();
-                        tvseries.setName(seriesName);
-                        tvseries.setStatus(seriesStatus);
-                        tvseries.setLink(seriesLink);
+                    while(seriesName == null){
+                        seriesName = JOptionPane.showInputDialog("Series' name: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
 
-                        fileHandler.JSONwriter(tvseries);  
                     }
+                    while(seriesStatusPre == null){
+                        seriesStatusPre = JOptionPane.showInputDialog("Series' status: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
+
+                    }
+                    while(seriesLink == null){
+                        seriesLink = JOptionPane.showInputDialog("Series' link: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
+
+                    }
+
+                    seriesStatus = Boolean.parseBoolean(seriesStatusPre);
+                    TVSeries tvseries = new TVSeries();
+                    tvseries.setName(seriesName);
+                    tvseries.setStatus(seriesStatus);
+                    tvseries.setLink(seriesLink);
+
+                    fileHandler.JSONwriter(tvseries);  
+                    
 		}
 		else if(e.getActionCommand().equals("Change")) {
                     String seriesName = JOptionPane.showInputDialog("Series' name: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
-                    String seriesStatus = JOptionPane.showInputDialog("Series' status: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
-                    fileHandler.JSONmod(seriesName, seriesStatus);
+                    String newSeriesStatus = JOptionPane.showInputDialog("Series' status: (ᵖʳᵉˢˢ﹡ᵗᵒ ᵃᵇᵒʳᵗ)");
+                    fileHandler.JSONmod(seriesName, newSeriesStatus);
 		}
                 
                 else if(e.getActionCommand().equals("About")){
                     JDialog dialog = new JDialog(this.mainWindow.mainFrame, "About");
-                    JTextArea dialogArea = new JTextArea("This is a simple Java Swing gui \n that uses JSON files to track \n my downloaded tv series." + "\n\n\n\n\n\n" + "            ©Cristian Di Iorio 2022");
+                    JTextArea dialogArea = new JTextArea("This is a simple Java Swing GUI \n that uses JSON files to track \n my downloaded tv series." + "\n\n\n\n\n\n" + "            ©Cristian Di Iorio 2022");
                     
                     dialogArea.setSize(190,190);
                     dialogArea.setEditable(false);
